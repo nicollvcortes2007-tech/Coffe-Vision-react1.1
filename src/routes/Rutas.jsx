7cbfa8b3dd8638f28faf5b482bfb5272b797175e
Yina Martinez <lizethmartinezguzman@.com>
2026-09-23 11:24:30 -0500

    Primera parte de Coffe+vision React

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -7,9 +7,11 @@ import Signup from "@/pages/Signup";
 
 // Páginas del caficultor
 import DashboardCaficultor from "@/pages/DashboardCaficultor";
+import Inicio from '@/pages/Inicio';
+import Recomendaciones from '@/pages/Recomendaciones';
 
 // Páginas del administrador
-
+import ConfiguracionAdministrador from "../pages/ConfiguracionAdministrador";
 
 function Rutas() {
   return (
@@ -21,6 +23,11 @@ function Rutas() {
 
       {/* Caficultor */}
       <Route path="/dashboard/caficultor" element={<DashboardCaficultor />} />
+      <Route path="inicio" element={<Inicio/>} />
+      <Route path="recomendaciones" element={<Recomendaciones />} />
+
+      {/* Administrador */}
+      <Route path='configuracion' element={<ConfiguracionAdministrador />} />
 
     </Routes>
   );
